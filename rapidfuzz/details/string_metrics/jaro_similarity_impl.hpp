d0564c412f395b5788ab316841f12802a58af0a5
Max Bachmann <kontakt@maxbachmann.de>
2021-04-04 17:19:33 +0200

    fix more things

--- rapidfuzz/details/string_metrics/jaro_similarity_impl.hpp
+++ rapidfuzz/details/string_metrics/jaro_similarity_impl.hpp
@@ -2,33 +2,19 @@
 /* Copyright © 2021 Max Bachmann */
 
 #include <rapidfuzz/details/common.hpp>
+#include <rapidfuzz/details/builtin.h>
 #include <numeric>
 #include <algorithm>
 #include <array>
 #include <limits>
 #include <iostream>
+#include <bitset>
+
+#undef NDEBUG
 
 namespace rapidfuzz {
 namespace string_metric {
 namespace detail {
-
-/*
- * count the number of bits set in a 64 bit integer
- * The code uses wikipedia's 64-bit popcount implementation:
- * http://en.wikipedia.org/wiki/Hamming_weight#Efficient_implementation
- */
-static inline std::size_t _popcount64(uint64_t x)
-{
-  const uint64_t m1  = 0x5555555555555555; //binary: 0101...
-  const uint64_t m2  = 0x3333333333333333; //binary: 00110011..
-  const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; //binary:  4 zeros,  4 ones ...
-  const uint64_t h01 = 0x0101010101010101; //the sum of 256 to the power of 0,1,2,3...
-
-  x -= (x >> 1) & m1;             //put count of each 2 bits into those 2 bits
-  x = (x & m2) + ((x >> 2) & m2); //put count of each 4 bits into those 4 bits
-  x = (x + (x >> 4)) & m4;        //put count of each 8 bits into those 8 bits
-  return static_cast<std::size_t>((x * h01) >> 56);  //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
-}
 
 template <typename T, typename U>
 constexpr T _bit_set(T val, U bit)
@@ -62,7 +48,7 @@ static inline double jaro_similarity_impl(basic_string_view<CharT1> s1, basic_st
   }
 
   /* this has to be handled here, since this would cause overflow issues in the implementation */
-  if (s2.size() < 2) {
+  if (s1.empty() || s2.size() < 2) {
     return 0;
   }
 
@@ -77,11 +63,8 @@ static inline double jaro_similarity_impl(basic_string_view<CharT1> s1, basic_st
   size_t matches = 0;        // no. of matched characters between s1 and s2
   size_t transpositions = 0; // no. of transpositions between s1 and s2
 
-  /* remove common prefix and count it as matches */
-  /* Suffix could be removed, but would lead to different transposition counts
-   * AFAIK it would be <= the current one, which would probably be a good thing.
-   * However it would lead to different results to the implementation this
-   * is tested against, which is why it is not done yet.
+  /* remove common prefix and count it as matches. The common prefix can not
+   * have any transpositions, so it is a lot faster to remove it beforehand
    */
   matches = rapidfuzz::common::remove_common_prefix(s1, s2);
 
@@ -94,67 +77,61 @@ static inline double jaro_similarity_impl(basic_string_view<CharT1> s1, basic_st
   uint64_t flagged_1 = 0; // positions in s1 which are matches to some character in s2
   uint64_t flagged_2 = 0; // positions in s2 which are matches to some character in s1
 
-  /* The upper bound of the distance for being a matched character.
-   * max(len_s1, len_s2)
+  /* Iterate through sequences, check for matches and compute transpositions.
+   * Two characters from s1 and s2 respectively, are considered matching only
+   * if they are the same and not farther than `max(len_s1, len_s2) / 2 - 1`
+   * characters apart.
+   * In this implementation this is achieved using a bitmasks, to ignore
+   * previous matches and characters outside of the search range.
    */
-  const size_t match_bound = s2.size() / 2 - 1;
 
-  auto match_character = [&](size_t s1_pos, uint64_t bound_mask) {
+  /* The upper bound of the distance for being a matched character.
+   * max(s1_full_len, s2_full_len). It is important to calculate the boundary using the 
+   * string length before the common prefix is removed, so the length change
+   * does not affect the search range.
+   */
+  const size_t match_bound = s2_full_len / 2 - 1;
+  /* the bound mask starts as 1^(match_bound+1) to cover both the current element
+   * and the match bound.
+   */
+  uint64_t bound_mask = ((uint64_t)1 << (match_bound+1)) - 1;
+
+  size_t s1_pos = 0;
+  /* for s1_pos < match_bound, the bitmask is shifted by one, setting the first bit to 1
+   * to cover the match bound in both directions of s1_pos
+   */
+  const size_t range_left = std::min(s1.size(), match_bound);
+  for(; s1_pos < range_left; s1_pos++) {
     uint64_t unused_matches = pattern.get(s1[s1_pos]) & ~flagged_2;
-
     unused_matches &= bound_mask;
 
     if (unused_matches) {
-      const size_t s2_pos = __builtin_ctzll(unused_matches);
+      const size_t s2_pos = psnip_builtin_ctz64(unused_matches);
       flagged_1 = _bit_set(flagged_1, s1_pos);
       flagged_2 = _bit_set(flagged_2, s2_pos);
       matches++;
     }
-  };
 
-  /* Iterate through sequences, check for matches and compute transpositions.
-   * jaro uses the following search range in s2:
-   * - upperbound = min(s1_pos + match_bound, len_s2 - 1)
-   * - lowerbound = max(0, s1_pos - match_bound)
-   *
-   *  The following implementation splits the iteration over s2 in sub ranges
-   *  so this lower/upperbound does not has to be computed and applied to the
-   *  bitvector in the inner loop
+    bound_mask = (bound_mask << 1) | 1;
+  }
+
+  /* for s1_pos >= match_bound the bitmask is only shifted by one, since the first
+   * character is now to far apart from s1_pos and should be ignored.
+   * We can ignore, that the bitmask covers characters beyond s2_len,
+   * since they do not exist in the PatternMatchVector
    */
-  if (s1.size() <= match_bound) {
-    /* upperbound is always len_s2 and lowerbound always 0, so it can be ignored */
-    for (size_t s1_pos = 0; s1_pos < s1.size(); s1_pos++) {
-      match_character(s1_pos, (uint64_t)-1);
-    }
-  } else {
-    /* the iteration can be divided into three ranges, in which lower/upperbound
-     * can be calculated without branches
-     */
-    size_t s1_pos = 0;
-    uint64_t lowerbound_mask = (uint64_t)-1;
-    uint64_t upperbound_mask = ((uint64_t)1 << match_bound) - 1;
+  for(; s1_pos < s1.size(); s1_pos++) {
+    uint64_t unused_matches = pattern.get(s1[s1_pos]) & ~flagged_2;
+    unused_matches &= bound_mask;
 
-    /* overlapping range left */
-    for (; s1_pos <= match_bound; s1_pos++) {
-      match_character(s1_pos, upperbound_mask);
-      upperbound_mask = (upperbound_mask << 1) | 1;
+    if (unused_matches) {
+      const size_t s2_pos = psnip_builtin_ctz64(unused_matches);
+      flagged_1 = _bit_set(flagged_1, s1_pos);
+      flagged_2 = _bit_set(flagged_2, s2_pos);
+      matches++;
     }
 
-    lowerbound_mask <<= 1;
-
-    /* overlapping range center (only exists for uneven string lengths)
-     */
-    if (s2.size() % 2) {
-      match_character(s1_pos, lowerbound_mask & upperbound_mask);
-      s1_pos++;
-      lowerbound_mask <<= 1;
-    }
-
-    /* overlapping range right */
-    for (; s1_pos < s1.size(); s1_pos++) {
-      match_character(s1_pos, lowerbound_mask);
-      lowerbound_mask <<= 1;
-    }
+    bound_mask <<= 1;
   }
 
   /* If no characters in common - return */
@@ -174,7 +151,7 @@ static inline double jaro_similarity_impl(basic_string_view<CharT1> s1, basic_st
           continue;
         }
 
-        const size_t index = __builtin_ctzll(flagged_2);
+        const size_t index = psnip_builtin_ctz64(flagged_2);
 
         if (s1[s1_pos] != s2[s2_pos + index]) {
           transpositions++;
@@ -231,6 +208,44 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
    */
   const size_t match_bound = s2.size() / 2 - 1;
 
+
+  auto match_character = [&s1, &flagged_2, &flagged_1, &pattern, &matches, words2](
+    size_t word1, size_t word1_pos, size_t s1_pos,
+    size_t lowerbound_words, uint64_t lowerbound_mask) __attribute__((always_inline))
+  {
+    {
+      /* clear character positions in s2 which already match a character in s1 */
+      uint64_t unused_matches = pattern.get(lowerbound_words, s1[s1_pos]) & ~flagged_2[lowerbound_words];
+
+      unused_matches &= lowerbound_mask;
+
+      if (unused_matches) {
+        const int index = psnip_builtin_ctz64(unused_matches) ;
+        flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
+        flagged_2[lowerbound_words] = _bit_set(flagged_2[lowerbound_words], index);
+        matches++;
+
+        /* after character is matched continue with the next */
+        return;
+      }
+    }
+
+    for (std::size_t word2 = lowerbound_words + 1; word2 < words2; word2++) {
+      /* clear character positions in s2 which already match a character in s1 */
+      const uint64_t unused_matches = pattern.get(word2, s1[s1_pos]) & ~flagged_2[word2];
+
+      if (unused_matches) {
+        const int word2_pos = psnip_builtin_ctz64(unused_matches) ;
+        flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
+        flagged_2[word2] = _bit_set(flagged_2[word2], word2_pos);
+        matches++;
+        /* after character is matched continue with the next */
+        break;
+      }
+    }
+  };
+
+
   /* Iterate through sequences, check for matches and compute transpositions.
    * jaro uses the following search range in s2:
    * - upperbound = min(s1_pos + match_bound, len_s2 - 1)
@@ -250,7 +265,7 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
         const uint64_t unused_matches = pattern.get(word2, s1[i]) & ~flagged_2[word2];
 
         if (unused_matches) {
-          const int index = __builtin_ctzll(unused_matches) ;
+          const int index = psnip_builtin_ctz64(unused_matches) ;
           flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
           flagged_2[word2] = _bit_set(flagged_2[word2], index);
           matches++;
@@ -263,6 +278,11 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
     /* the iteration can be divided into two ranges, in which lower/upperbound
      * can be calculated without branches
      */
+
+
+    //size_t upperbound_words = match_bound / 64;
+    //uint64_t upperbound_mask = ((uint64_t)1 << (match_bound % 64)) - 1;
+
     /* overlapping range left */
     for (size_t i = 0; i <= match_bound; i++) {
       const uint64_t word1 = i / 64;
@@ -281,7 +301,7 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
         const uint64_t unused_matches = pattern.get(word2, s1[i]) & ~flagged_2[word2];
 
         if (unused_matches) {
-          const int index = __builtin_ctzll(unused_matches) ;
+          const int index = psnip_builtin_ctz64(unused_matches) ;
           flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
           flagged_2[word2] = _bit_set(flagged_2[word2], index);
           matches++;
@@ -292,10 +312,12 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
 
       if (upperbound_pos) {
         // clear character positions in s2 which already match a character in s1
-        const uint64_t unused_matches = pattern.get(upperbound_words, s1[i]) & ~flagged_2[upperbound_words];
+        uint64_t unused_matches = pattern.get(upperbound_words, s1[i]) & ~flagged_2[upperbound_words];
+
+        //unused_matches &= upperbound_mask;
 
         if (unused_matches) {
-          const int index = __builtin_ctzll(unused_matches);
+          const int index = psnip_builtin_ctz64(unused_matches);
           if (index <= upperbound_pos) {
             flagged_1[word1] = _bit_set(flagged_1[word1], i);
             flagged_2[upperbound_words] = _bit_set(flagged_2[upperbound_words], index);
@@ -303,18 +325,31 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
           }
         }
       }
+
+      /*upperbound_mask = (upperbound_mask << 1) | 1;
+      if(!(~upperbound_mask)) {
+        upperbound_words++;
+        upperbound_mask = 0;
+      }*/
     }
+
+
+    size_t lowerbound_words = 0;
+    uint64_t lowerbound_mask = (uint64_t)-1 << 1;
 
     /* overlapping range right */
     for (size_t i = match_bound + 1; i < s1.size(); i++) {
       const uint64_t word1 = i / 64;
       const uint64_t word1_pos = i % 64;
+#if 0
+      match_character(word1, word1_pos, i, lowerbound_words, lowerbound_mask);
 
-      /* clear non search range on the left */
-      const size_t lowerbound = i - match_bound;
-      const size_t lowerbound_words = lowerbound / 64;
-      const size_t lowerbound_pos = lowerbound % 64;
-
+      lowerbound_mask <<= 1;
+      if (!lowerbound_mask) {
+        lowerbound_words++;
+        lowerbound_mask = (uint64_t)-1;
+      }
+#else
       /* lowerbound only has to be cleared in the first word
        * it is guaranteed, that words2 > lowerbound_words
        */
@@ -322,15 +357,20 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
         /* clear character positions in s2 which already match a character in s1 */
         uint64_t unused_matches = pattern.get(lowerbound_words, s1[i]) & ~flagged_2[lowerbound_words];
 
-        /* at most shift by 63 */
-        unused_matches >>= lowerbound_pos;
-        unused_matches <<= lowerbound_pos;
+        unused_matches &= lowerbound_mask;
 
         if (unused_matches) {
-          const int index = __builtin_ctzll(unused_matches) ;
+          const int index = psnip_builtin_ctz64(unused_matches) ;
           flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
           flagged_2[lowerbound_words] = _bit_set(flagged_2[lowerbound_words], index);
           matches++;
+
+          lowerbound_mask <<= 1;
+          if (!lowerbound_mask) {
+            lowerbound_words++;
+            lowerbound_mask = (uint64_t)-1;
+          }
+
           /* after character is matched continue with the next */
           continue;
         }
@@ -341,15 +381,21 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
         const uint64_t unused_matches = pattern.get(word2, s1[i]) & ~flagged_2[word2];
 
         if (unused_matches) {
-          const int index = __builtin_ctzll(unused_matches) ;
+          const int index = psnip_builtin_ctz64(unused_matches) ;
           flagged_1[word1] = _bit_set(flagged_1[word1], word1_pos);
           flagged_2[word2] = _bit_set(flagged_2[word2], index);
           matches++;
-          /* todo potentially faster to have a counter in here instead of words1 * bitcount */
           /* after character is matched continue with the next */
           break;
         }
       }
+
+      lowerbound_mask <<= 1;
+      if (!lowerbound_mask) {
+        lowerbound_words++;
+        lowerbound_mask = (uint64_t)-1;
+      }
+#endif
     }
   }
 
@@ -357,25 +403,26 @@ static inline double jaro_similarity_impl_block(basic_string_view<CharT1> s1, ba
   if (!matches) {
     return 0;
   }
+  return matches;
 
     /* Count the number of transpositions */
     /* todo this can definetly be done faster */
   {
     size_t k = 0;
-    for (size_t i = 0; i < s1.size(); i++) {
-      const uint64_t word1 = i / 64;
-      const uint64_t word1_pos = i % 64;
+    for (size_t s1_pos = 0; s1_pos < s1.size(); s1_pos++) {
+      const uint64_t word1 = s1_pos / 64;
+      const uint64_t word1_pos = s1_pos % 64;
       if (_bit_check(flagged_1[word1], word1_pos)) {
-        size_t j = k;
-        for (; j < s2.size(); j++) {
-          const uint64_t word2 = j / 64;
-          const uint64_t word2_pos = j % 64;
+        size_t s2_pos = k;
+        for (; s2_pos < s2.size(); s2_pos++) {
+          const uint64_t word2 = s2_pos / 64;
+          const uint64_t word2_pos = s2_pos % 64;
           if (_bit_check(flagged_2[word2], word2_pos)) {
-            k = j + 1;
+            k = s2_pos + 1;
             break;
           }
         }
-        if (s1[i] != s2[j]) {
+        if (s1[s1_pos] != s2[s2_pos]) {
           transpositions++;
         }
       }
